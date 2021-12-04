7fe5fe26174efdfdc870c84721bd0c8ea492fe32
Vlada <Vlada.Khaustova@yandex.ru>
2021-12-04 22:14:56 +0400

    Added selection sort

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import json
 
+def selection_sort(data, key):
+    n = len(data)
+    for i in range(n-1):
+        m = data[i][key]
+        p = i
+        for j in range(i+1, n):
+            if m > data[j][key]:
+                m = data[j][key]
+                p = j
+        if p != i:
+            t = data[i][key]
+            data[i][key] = data[p][key]
+            data[p][key] = t
+
+
+data = json.load(open("valid_data.txt", encoding="windows-1251"))
+selection_sort(data, 'height')
+
+json.dump(data, open("result_sort.json", "w", encoding="windows-1251"), ensure_ascii=False, indent=5)
+result = json.load(open("result_sort.json", encoding="windows-1251"))
+print(result)
